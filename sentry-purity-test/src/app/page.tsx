408c2abe332e3016d8479ce367321aef632b43d2
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2025-08-18 14:32:00 -0700

    lint

--- sentry-purity-test/src/app/page.tsx
+++ sentry-purity-test/src/app/page.tsx
@@ -28,11 +28,12 @@ export default function Home() {
     <div className="description">
        Welcome to the Sentry Purity Test. This is your official (not really) 
        employee guide to the rites of passage that make you a true Sentaur™.
-       Whether you're a fresh-faced newbie still figuring out how to use Okta, 
-       or a battle-scarred senior who's stolen enough protein bars to feed a small country,
-       let's see how "pure" you really are.
+       Whether you&apos;re a fresh-faced newbie still figuring out how to use Okta, 
+       or a battle-scarred senior who&apos;s stolen enough protein bars to feed a small country,
+       let&apos;s see how &quot;pure&quot; you really are.
        <br></br><br></br>
-       Remember: there's no shame in admitting you've done these things. The shame is in pretending you haven't. 
+       Remember: there&apos;s no shame in admitting you&apos;ve done these things. 
+       The shame is in pretending you haven&apos;t. 
        <br></br><br></br>
        <em>Disclaimer: This test may cause unsettling war flashbacks. 
         Whether to treat this as a bucket list is up to your poor judgment. </em>
