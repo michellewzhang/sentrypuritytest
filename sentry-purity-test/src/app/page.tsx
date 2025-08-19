a404f0ddef5073b3b59644b795c254b1a3d9495a
Michelle Fu <michelle.fu@sentry.io>
2025-08-19 12:03:15 -0700

    smol changes

--- sentry-purity-test/src/app/page.tsx
+++ sentry-purity-test/src/app/page.tsx
@@ -34,14 +34,14 @@ export default function Home() {
        Welcome to the Sentry Purity Test. This is your official (not really) 
        employee guide to the rites of passage that make you a true Sentaur™.
        Whether you&apos;re a fresh-faced newbie still figuring out how to use Okta, 
-       or a battle-scarred senior who&apos;s stolen enough protein bars to feed a small country,
+       or a battle-scarred senior who&apos;s stolen enough sparkling water to feed a small country,
        let&apos;s see how &quot;pure&quot; you really are.
        <br></br><br></br>
        Remember: there&apos;s no shame in admitting you&apos;ve done these things. 
        The shame is in pretending you haven&apos;t. 
        <br></br><br></br>
        <em>Disclaimer: This test may cause unsettling war flashbacks. 
-        Whether to treat this as a bucket list is up to your poor judgment. </em>
+        Whether or not to treat this as a bucket list is up to your poor judgment. </em>
      </div>
 
     <div className="list-header">Have you ever...</div>
